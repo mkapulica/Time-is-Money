18f73179825e2f12de70d28bec4a4fee39ea9445
Mislav Kapulica <mislav@kapulica.eu>
2023-08-28 22:41:55 +0200

    fix: Optimization and refactoring

--- content.js
+++ content.js
@@ -1,153 +1,119 @@
-// The regular expression for price detection.
-const priceRegex = /(?:(?<=\s)|^)(?:(?:[£€$¥₹₽]|Rs\.?)\s*\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{0,2})?|\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{0,2})?\s*(?:[£€$¥₹₽]|Rs\.?))(?:(?=\s)|$)/g;
-
-const conversionRatesToEUR = {
-    '£': 0.85,   // example rate for GBP to EUR
-    '€': 1,     // EUR to EUR is 1:1
-    '$': 0.90,  // example rate for USD to EUR
-    '¥': 0.008, // example rate for JPY to EUR
-    '₹': 0.011, // example rate for INR to EUR
-    '₽': 0.011, // example rate for RUB to EUR
-    'Rs': 0.011 // treating Rs also as INR for this example
+const PRICE_REGEX = /(?:(?<=\s)|^)(?:(?:[£€$¥₹₽]|Rs\.?)\s*\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{0,2})?|\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{0,2})?\s*(?:[£€$¥₹₽]|Rs\.?))(?:(?=\s)|$)/g;
+const CONVERSION_RATES_TO_EUR = {
+    '£': 0.85, '€': 1, '$': 0.90, '¥': 0.008, '₹': 0.011, '₽': 0.011, 'Rs': 0.011
 };
+const DAYS_IN_YEAR = 365.25;
 
 let hourWorth = null;
 
 async function refreshHourWorth() {
-  hourWorth = await getHourWorth();
+    hourWorth = await getHourWorthFromStorage();
 }
 
-// Get settings from browser storage
-function getHourWorth() {
-  return new Promise((resolve, reject) => {
-      browser.storage.local.get('settings', function(data) {
-          if (data.settings) {
-              var monthlyIncome = Number(data.settings.monthlyIncome);
-              var weeklyWorkdays = Number(data.settings.weeklyWorkdays);
-              var dailyWorkHours = Number(data.settings.dailyWorkHours);
-              var dailyCommuteMinutes = Number(data.settings.dailyCommuteMinutes);
-              var monthlyCommuteCost = Number(data.settings.monthlyCommuteCost);
-              var vacationDays = Number(data.settings.vacationDays);
+async function getHourWorthFromStorage() {
+    return new Promise((resolve, reject) => {
+        browser.storage.local.get('settings', data => {
+            if (data.settings) {
+                resolve(computeHourWorth(data.settings));
+            } else {
+                reject("No settings found.");
+            }
+        });
+    });
+}
 
-              var monthlyWorkHours = ((365.25 - vacationDays) / 12.0 / 7.0) * weeklyWorkdays * (dailyWorkHours + (dailyCommuteMinutes / 60.0));
+function computeHourWorth(settings) {
+    const {
+        monthlyIncome, weeklyWorkdays, dailyWorkHours,
+        dailyCommuteMinutes, monthlyCommuteCost, vacationDays
+    } = settings;
 
-              var hourWorth = (monthlyIncome - monthlyCommuteCost) / monthlyWorkHours;
+    const monthlyWorkHours = computeMonthlyWorkHours(weeklyWorkdays, dailyWorkHours, dailyCommuteMinutes, vacationDays);
+    return (monthlyIncome - monthlyCommuteCost) / monthlyWorkHours;
+}
 
-              resolve(hourWorth);  // Resolve the promise with hourWorth
-          } else {
-              reject("No settings found.");  // Reject if no settings
-          }
-      });
-  });
+function computeMonthlyWorkHours(weeklyWorkdays, dailyWorkHours, dailyCommuteMinutes, vacationDays) {
+    return ((DAYS_IN_YEAR - vacationDays) / 12 / 7) * weeklyWorkdays * (dailyWorkHours + (dailyCommuteMinutes / 60));
 }
 
 function parsePrice(priceStr) {
-    let currencySymbol = priceStr.match(/[£€$¥₹₽]|Rs\.?/)[0];
-    // Remove any currency symbols.
-    let cleaned = priceStr.replace(/[£€$¥₹₽]|Rs\.?/g, "").trim();
-    
-    if (cleaned.includes(".") && cleaned.includes(",")) {
-        if (cleaned.lastIndexOf(".") > cleaned.lastIndexOf(",")) {
-            // Dot is the decimal, comma is the thousands separator.
-            cleaned = cleaned.replace(/,/g, "");
-        } else {
-            // Comma is the decimal, dot is the thousands separator.
-            cleaned = cleaned.replace(/\./g, "").replace(/,/g, ".");
-        }
-    } else if (cleaned.includes(",")) {
-        if ((cleaned.match(/,/g) || []).length > 1 || cleaned.endsWith(",")) {
-            // Comma is the thousands separator.
-            cleaned = cleaned.replace(/,/g, "");
-        } else {
-            // Comma is the decimal separator.
-            cleaned = cleaned.replace(/,/g, ".");
-        }
-    } else if (cleaned.includes(".")) {
-        const dotPosition = cleaned.indexOf(".");
-        if ((cleaned.match(/\./g) || []).length > 1 || cleaned.endsWith(".") || cleaned.substring(dotPosition + 1).length > 2) {
-            // Dot is the thousands separator.
-            cleaned = cleaned.replace(/\./g, "");
-        }
-        // If it's just one dot, not at the end, and less than 3 digits after it, it's a decimal.
-    }
-
+    const [currencySymbol] = priceStr.match(/[£€$¥₹₽]|Rs\.?/) || [null];
+    let cleaned = cleanPriceString(priceStr);
     return {
         value: parseFloat(cleaned),
         currency: currencySymbol
     };
 }
 
-function convertToEUR(amount, currency) {
-    if (conversionRatesToEUR[currency]) {
-        return amount * conversionRatesToEUR[currency];
-    }
-    return amount; // If no conversion rate found, return the original amount (this could be improved)
+function cleanPriceString(priceStr) {
+  let cleaned = priceStr.replace(/[£€$¥₹₽]|Rs\.?/g, "").trim();
+  const delimiters = new Set(cleaned.match(/[.,]/g));
+
+  if (delimiters.has('.')) {
+      if (cleaned.indexOf('.') !== cleaned.lastIndexOf('.') || cleaned.endsWith('.')) {
+          cleaned = cleaned.replace(/\./g, "");
+      }
+      if (cleaned.lastIndexOf(".") < cleaned.lastIndexOf(",")) {
+          cleaned = cleaned.replace(/\./g, "").replace(/,/g, ".");
+      }
+  }
+
+  if (delimiters.has(',')) {
+      if (cleaned.indexOf(',') !== cleaned.lastIndexOf(',') || cleaned.endsWith(',')) {
+          cleaned = cleaned.replace(/,/g, "");
+      } else {
+          cleaned = cleaned.replace(/,/g, ".");
+      }
+  }
+
+  return cleaned;
 }
 
-/**
- * Substitutes replaced price text into text nodes.
- * If the node contains more than just text (ex: it has child nodes),
- * call replaceText() on each of its children.
- *
- * @param  {Node} node    - The target DOM Node.
- * @return {void}         - Note: the price substitution is done inline.
- */
+function convertToEUR(amount, currency) {
+    return amount * (CONVERSION_RATES_TO_EUR[currency] || 1);
+}
+
 async function replaceText(node) {
-  if (node.nodeType === Node.TEXT_NODE) {
-    if (node.parentNode && node.parentNode.nodeName === 'TEXTAREA') {
-      return;
-    }
+  if (node.nodeType === Node.TEXT_NODE && !(node.parentNode && node.parentNode.nodeName === 'TEXTAREA')) {
+      const originalContent = node.dataOriginalContent || node.textContent;
+      
+      const replacedContent = originalContent.replace(PRICE_REGEX, match => {
+          const { value, currency } = parsePrice(match);
+          const convertedValue = convertToEUR(value, currency) / hourWorth;
+          return `${convertedValue.toFixed(2)} h`;
+      });
 
-    // Check if we've stored the original content before
-    const originalContent = node.dataOriginalContent || node.textContent;
-    node.dataOriginalContent = originalContent;  // Store the original content
-
-    // Replace every occurrence of 'price' in 'content' with its "Replaced" tag.
-    let content = originalContent.replace(priceRegex, (match) => {
-        const parsed = parsePrice(match);
-        const value = convertToEUR(parsed.value, parsed.currency);
-        const convertedValue = value / hourWorth;  // Example conversion
-        return convertedValue.toFixed(2).toString() + " h";
-    });
-
-    node.textContent = content;
+      if (replacedContent !== originalContent) {
+          node.dataOriginalContent = originalContent;
+          node.textContent = replacedContent;
+      }
   } else {
-    for (let i = 0; i < node.childNodes.length; i++) {
-      replaceText(node.childNodes[i]);
-    }
+      Array.from(node.childNodes).forEach(replaceText);
   }
 }
 
 async function changeValues() {
-  await refreshHourWorth();
-  // Start the recursion from the body tag.
-  replaceText(document.body);
+    await refreshHourWorth();
+    replaceText(document.body);
 }
 
 changeValues();
 
-browser.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
-  if (request.action === "settingsUpdated") {
-      // Fetch the new settings
-      const data = await browser.storage.local.get('settings');
-      if (data.settings) {
-          await changeValues();
-      }
-  }
+browser.runtime.onMessage.addListener(async request => {
+    if (request.action === "settingsUpdated") {
+        const data = await browser.storage.local.get('settings');
+        if (data.settings) {
+            await changeValues();
+        }
+    }
 });
 
-// Now monitor the DOM for additions and replace prices in new nodes.
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-      for (let i = 0; i < mutation.addedNodes.length; i++) {
-        const newNode = mutation.addedNodes[i];
-        replaceText(newNode);
-      }
-    }
-  });
+const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        if (mutation.addedNodes) {
+            Array.from(mutation.addedNodes).forEach(replaceText);
+        }
+    });
 });
-observer.observe(document.body, {
-  childList: true,
-  subtree: true
-});
+
+observer.observe(document.body, { childList: true, subtree: true });
--- popup.js
+++ popup.js
@@ -1,34 +1,64 @@
-document.getElementById('save').addEventListener('click', function() {
-    const settings = {
-        monthlyIncome: document.getElementById('monthlyIncome').value,
-        weeklyWorkdays: document.getElementById('weeklyWorkdays').value,
-        dailyWorkHours: document.getElementById('dailyWorkHours').value,
-        dailyCommuteMinutes: document.getElementById('dailyCommuteMinutes').value,
-        monthlyCommuteCost: document.getElementById('monthlyCommuteCost').value,
-        vacationDays: document.getElementById('vacationDays').value,
-        onOff: document.getElementById('onOffSwitch').checked
-    };
+const SETTINGS_KEY = 'settings';
+const SETTING_NAMES = [
+    'monthlyIncome', 
+    'weeklyWorkdays', 
+    'dailyWorkHours', 
+    'dailyCommuteMinutes', 
+    'monthlyCommuteCost', 
+    'vacationDays'
+];
 
-    // Save settings to browser storage
-    browser.storage.local.set({settings: settings}, function() {
-        // Send a message to content.js once settings are saved
-        browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            browser.tabs.sendMessage(tabs[0].id, {action: "settingsUpdated"});
-        });
+function isCheckbox(element) {
+    return element.type === 'checkbox';
+}
+
+function getSettingValue(settingName) {
+    const element = document.getElementById(settingName);
+    if (isCheckbox(element)) {
+        return element.checked;
+    }
+    return Number(element.value);
+}
+
+function setSettingValue(settingName, value) {
+    const element = document.getElementById(settingName);
+    if (isCheckbox(element)) {
+        element.checked = value;
+    } else {
+        element.value = value;
+    }
+}
+
+function gatherSettings() {
+    return SETTING_NAMES.concat(['onOffSwitch']).reduce((settings, name) => {
+        settings[name] = getSettingValue(name);
+        return settings;
+    }, {});
+}
+
+function saveSettingsToStorage(settings) {
+    browser.storage.local.set({ [SETTINGS_KEY]: settings }, notifyContentScript);
+}
+
+function notifyContentScript() {
+    browser.tabs.query({ active: true, currentWindow: true }, tabs => {
+        browser.tabs.sendMessage(tabs[0].id, { action: "settingsUpdated" });
     });
-});
+}
 
-// Load saved settings when the popup is opened
-document.addEventListener('DOMContentLoaded', function() {
-    browser.storage.local.get('settings', function(data) {
-        if (data.settings) {
-            document.getElementById('monthlyIncome').value = data.settings.monthlyIncome;
-            document.getElementById('weeklyWorkdays').value = data.settings.weeklyWorkdays;
-            document.getElementById('dailyWorkHours').value = data.settings.dailyWorkHours;
-            document.getElementById('dailyCommuteMinutes').value = data.settings.dailyCommuteMinutes;
-            document.getElementById('monthlyCommuteCost').value = data.settings.monthlyCommuteCost;
-            document.getElementById('vacationDays').value = data.settings.vacationDays;
-            document.getElementById('onOffSwitch').checked = data.settings.onOff;
+function applyStoredSettingsToPage() {
+    browser.storage.local.get(SETTINGS_KEY, data => {
+        if (data[SETTINGS_KEY]) {
+            Object.keys(data[SETTINGS_KEY]).forEach(name => {
+                setSettingValue(name, data[SETTINGS_KEY][name]);
+            });
         }
     });
+}
+
+document.getElementById('save').addEventListener('click', () => {
+    const settings = gatherSettings();
+    saveSettingsToStorage(settings);
 });
+
+document.addEventListener('DOMContentLoaded', applyStoredSettingsToPage);
